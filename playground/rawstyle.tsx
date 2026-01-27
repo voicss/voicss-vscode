6c495e82db14ec1356819f06ef404501dedaa832
kh4f <kh4f.dev@gmail.com>
2026-01-27 22:56:35 +0300

    chore(playground): add rawstyle and vanilla CSS examples
    Add `playground/rawstyle.tsx` and `playground/vanilla.css` for testing and demonstrating CSS highlighting and injection.

---
+++ playground/rawstyle.tsx
@@ -0,0 +1,53 @@
+import { css, gcss } from 'rawstyle'
+
+void css`
+	font-size: 14rem;
+	font-family: "Tiny5", sans-serif;
+	color: var(--foreground);
+	text-align: center;
+	text-transform: uppercase;
+	letter-spacing: -1rem;
+	line-height: 0.7;
+	
+	.link {
+		font-family: system-ui;
+		font-size: 1.2rem;
+
+		& > a {
+			color: tomato;
+				&:hover, &:focus {
+				color: ivory;
+				background-color: tomato;
+			}
+		}
+	}
+`
+
+void gcss`
+	:root {
+		--background: #ebebeb;
+		--foreground: #303030;
+	}
+
+	body {
+		background-color: var(--background);
+		height: 100vh;
+		overflow: hidden;
+		margin: 0;
+		
+		&.dark {
+			--background: #303030;
+			--foreground: #ebebeb;
+		}
+		
+		main {
+			display: flex;
+			justify-content: center;
+			align-items: center;
+			
+			&:hover {
+				cursor: default;
+			}
+		}
+	}
+`
