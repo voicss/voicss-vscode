153427d98317d073cdf4f1bb5de225fe0b693e5e
kh4f <kh4f.dev@gmail.com>
2026-01-28 23:14:58 +0300

    chore(package): remove unused React dependencies and settings
    - Remove unused React-related dependencies from `package.json`
    - Remove React-specific ESLint rules and plugins from `eslint.config.ts`
    - Remove `jsx` setting from `tsconfig.json`

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,10 +1,6 @@
 import { defineConfig, globalIgnores } from 'eslint/config'
 import eslint from '@eslint/js'
 import tseslint from 'typescript-eslint'
-import eslintReact from '@eslint-react/eslint-plugin'
-import reactPlugin from 'eslint-plugin-react'
-import reactHooks from 'eslint-plugin-react-hooks'
-import jsxA11y from 'eslint-plugin-jsx-a11y'
 import stylistic from '@stylistic/eslint-plugin'
 
 export default defineConfig([
@@ -23,18 +19,6 @@ export default defineConfig([
 			'@typescript-eslint/no-confusing-void-expression': ['error', { ignoreArrowShorthand: true }],
 			'@typescript-eslint/restrict-template-expressions': 'off',
 		},
-	},
-	{
-		name: 'React Rules',
-		files: ['**/*.tsx'],
-		settings: { react: { version: 'detect' } },
-		extends: [
-			jsxA11y.flatConfigs.strict,
-			reactPlugin.configs.flat.recommended,
-			reactPlugin.configs.flat['jsx-runtime'],
-			reactHooks.configs.flat.recommended,
-			eslintReact.configs['recommended-type-checked'],
-		],
 	},
 	{
 		name: 'Stylistic Rules',
